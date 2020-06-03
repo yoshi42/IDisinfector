c147928e9bced70f99123252f710c3a2bca47ca4
Bohdan <bodyarudyk@gmail.com>
2020-06-03 14:44:07 +0300

    main code

---
+++ Idisinfector_main/Idisinfector_main.ino
@@ -0,0 +1,41 @@
+#define sensor0 A0
+#define sensor1 A1
+#define actuator 3
+#define LED 13
+
+bool flag = false;
+
+void setup()
+{
+	pinMode(sensor0, INPUT_PULLUP);
+	pinMode(sensor1, INPUT_PULLUP);
+	pinMode(actuator, OUTPUT);
+	digitalWrite(actuator, LOW);
+	pinMode(LED, OUTPUT);
+	digitalWrite(LED, HIGH);
+	Serial.begin(9600);
+	Serial.println("Started");
+}
+
+void loop()
+{
+	Serial.print(analogRead(sensor0));
+	Serial.print("  ");
+	Serial.println(analogRead(sensor1));
+	delay(100);
+
+	if((analogRead(sensor0) <= 255 || analogRead(sensor1) <= 255) && flag == false)
+	{
+		delay(300);
+		analogWrite(actuator, 255);
+		delay(140);
+		analogWrite(actuator, 0);
+		flag = true;
+	}
+
+	else if (((analogRead(sensor0) >= 255 && analogRead(sensor1) >= 255) && flag == true))
+	{
+		flag = false;
+		delay(500);
+	}
+}
