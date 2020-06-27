a9fc6c3c20e1f78bdf92ccbbbb0e4e39c1fc2b62
yoshi42 <bodyarudyk@gmail.com>
2020-06-27 22:18:30 +0300

    Button, -clean machine, new sens
    Add a pump-on button, deleted a clean machine, new PNP-sensor instead of NPN

--- Idisinfector_main/Idisinfector_main.ino
+++ Idisinfector_main/Idisinfector_main.ino
@@ -1,18 +1,16 @@
-#define sensor0 A0
-#define sensor1 A1
+#define sens_disinfect A1 //not used in last red
 #define actuator 3
-#define LED 13
+#define button 4
 
-bool flag = false;
+bool flag_dis = false;
 
 void setup()
 {
-	pinMode(sensor0, INPUT_PULLUP);
-	pinMode(sensor1, INPUT_PULLUP);
+	pinMode(sens_disinfect, INPUT);
+	pinMode(button, INPUT_PULLUP);
 	pinMode(actuator, OUTPUT);
 	digitalWrite(actuator, LOW);
-	pinMode(LED, OUTPUT);
-	digitalWrite(LED, HIGH);
+
 	Serial.begin(9600);
 	Serial.println("Started");
 }
@@ -20,19 +18,29 @@ void setup()
 void loop()
 {
 	delay(200);
-
-	if((analogRead(sensor0) <= 255 || analogRead(sensor1) <= 255) && flag == false)
+	Serial.println(analogRead(sens_disinfect));
+	if((analogRead(sens_disinfect) >= 255) && flag_dis == false)
 	{
 		delay(300);
 		analogWrite(actuator, 255);
-		delay(30);
+		delay(10);
 		analogWrite(actuator, 0);
-		flag = true;
+		flag_dis = true;
+	}
+	else if ((analogRead(sens_disinfect) <= 255 && flag_dis == true))
+	{
+		flag_dis = false;
+		delay(500);
 	}
 
-	else if (((analogRead(sensor0) >= 255 && analogRead(sensor1) >= 255) && flag == true))
+	if(digitalRead(button) == LOW)
 	{
-		flag = false;
-		delay(500);
+		delay(100);
+		analogWrite(actuator, 255);
+	}
+	else if(digitalRead(button) == HIGH)
+	{
+		delay(100);
+		analogWrite(actuator, 0);
 	}
 }
